505d182e3151ed7eed32e3cded4633cc92e6bed9
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-04 23:49:21 +0200

    added dlobal state, full new job

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 console.log("Started ToDo app.");           //W4D4 created
+//app holds our global state
+const app = {
+    jobId : 0,
+    mybtn : document.querySelector("#btn1"),
+    todoCont : document.querySelector("#todos-cont")
+} 
 main();
 
+
 function main() {                           //W4D4 (1:15:)
-    const app = {
-        lastId : 0,
-        mybtn : document.querySelector("#btn1"),
-        todoCont : document.querySelector("#todos-cont")
-    } 
+
 
     //we create an event handler for add button
     app.mybtn.onclick = (event) => {               //W4D4 (:34:)
@@ -21,16 +24,27 @@ function main() {                           //W4D4 (1:15:)
     }
 }
 
-
-
  function addTodo(parent, value) {           //W4D4 (:53:)
     //Create New Todo
-    const newTodo = document.createElement("div");//W4D4 (1:13:)         
+    const newTodo = document.createElement("div");//W4D4 (1:13:);updated:(1:23:);
+    newTodo.classList.add("job-cont-"+app.jobId);
     newTodo.innerHTML = `                      
-        <input type="checkbox" name="" id="j-chk-1">
-        <span class="job-desc" id="j-desc-1">Buy Milk</span>
-        <button class="del-btn" id="j-del-1">DELETE</button>
+        <input type="checkbox" name="" id="j-chk-${app.jobId}">
+        <span class="job-desc" id="j-desc-${app.jobId}"></span>
+        <button class="del-btn" id="j-del-${app.jobId}">DELETE</button>
         `;
+    //newTodo only has one span
+    const jdesc = newTodo.querySelector("span");
+    jdesc.innerText = value;
+
     parent.appendChild(newTodo);
+
+    
+
+
+    //TODO move updating to separate function
+    app.jobId++;
+    //add Handlers
+    //ToDo
 }
 
