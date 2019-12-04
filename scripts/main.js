917ae8c9bdd675b3d5c4437385120bc3b5d981e6
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-04 20:46:48 +0200

    added action to field und button

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,9 @@ mybtn.onclick = (event) => {               //W4D4 (:34:)
     const inputField = document.querySelector("#myinput");
     console.log("Input value is: " + inputField.value);
 
-
+    //Add new ToDo: Create NewEl-->DabutSaturs->PieliktBernu
+    const newTodo = document.createElement("div");           //W4D4 (:40:)
+    newTodo.innerText = inputField.value;
+    const todoCont = document.querySelector("#todos-cont");
+    todoCont.appendChild(newTodo);
 }
