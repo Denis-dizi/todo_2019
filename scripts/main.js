6de1775534d61d280a8b5b8d06677b139ad35e02
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-04 23:16:47 +0200

    refact. with app container and main function

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,36 @@
 console.log("Started ToDo app.");           //W4D4 created
+main();
 
-const mybtn = document.querySelector("#btn1");
+function main() {                           //W4D4 (1:15:)
+    const app = {
+        lastId : 0,
+        mybtn : document.querySelector("#btn1"),
+        todoCont : document.querySelector("#todos-cont")
+    } 
 
-//we create an event handler for add button
-mybtn.onclick = (event) => {               //W4D4 (:34:)
-    console.log("You pressed ADD btn");
-    const inputField = document.querySelector("#myinput");
-    console.log("Input value is: " + inputField.value);
+    //we create an event handler for add button
+    app.mybtn.onclick = (event) => {               //W4D4 (:34:)
+        console.log("You pressed ADD btn");
+        const inputField = document.querySelector("#myinput");
+        console.log("Input value is: " + inputField.value);
 
-    //Add new ToDo: Create NewEl-->DabutSaturs->PieliktBernu  //W4D4 (:40:)
-    const todoCont = document.querySelector("#todos-cont");
-    
-    addTodo(todoCont, inputField.value);
+        //Add new ToDo: Create NewEl-->DabutSaturs->PieliktBernu  //W4D4 (:40:)
+       
+        
+        addTodo(app.todoCont, inputField.value);
+    }
 }
+
+
 
  function addTodo(parent, value) {           //W4D4 (:53:)
-    const newTodo = document.createElement("div");         
-    newTodo.innerText = value;
+    //Create New Todo
+    const newTodo = document.createElement("div");//W4D4 (1:13:)         
+    newTodo.innerHTML = `                      
+        <input type="checkbox" name="" id="j-chk-1">
+        <span class="job-desc" id="j-desc-1">Buy Milk</span>
+        <button class="del-btn" id="j-del-1">DELETE</button>
+        `;
     parent.appendChild(newTodo);
 }
+
