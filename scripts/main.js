1634f53e9ed7ad88641ade752b7723ad80f5fb87
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-04 20:45:41 +0200

    added field und button

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log("Started ToDo app.");
+console.log("Started ToDo app.");           //W4D4 created
+
+const mybtn = document.querySelector("#btn1");
+
+//we create an event handler for add button
+mybtn.onclick = (event) => {               //W4D4 (:34:)
+    console.log("You pressed ADD btn");
+    const inputField = document.querySelector("#myinput");
+    console.log("Input value is: " + inputField.value);
+
+
+}
